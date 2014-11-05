c141c0febbde12ac338f5af817b70bdfe0b08d8a
macguy8 <macfinder1@me.com>
2014-11-04 22:22:51 -0500

    More changes!

---
+++ src/main/java/net/frozenorb/foxtrot/command/commands/KOTHRewardKeyCommand.java
@@ -0,0 +1,7 @@
+package net.frozenorb.foxtrot.command.commands;
+
+/**
+ * Created by macguy8 on 11/4/2014.
+ */
+public class KOTHRewardKeyCommand {
+}
---
+++ src/main/java/net/frozenorb/foxtrot/command/commands/subcommands/kothsubcommands/KOTHSetTier.java
@@ -0,0 +1,7 @@
+package net.frozenorb.foxtrot.command.commands.subcommands.kothsubcommands;
+
+/**
+ * Created by macguy8 on 11/4/2014.
+ */
+public class KOTHSetTier {
+}
