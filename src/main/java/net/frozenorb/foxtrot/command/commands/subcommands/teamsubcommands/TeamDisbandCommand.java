c3263ab6e2c784209d06ac5d9809788be4c45c8b
macguy8 <macfinder1@me.com>
2014-11-16 12:59:18 -0500

    Refactor commands' classes to better reflect the command. [Part 2]

---
+++ src/main/java/net/frozenorb/foxtrot/command/commands/subcommands/teamsubcommands/TeamDisbandCommand.java
@@ -0,0 +1,42 @@
+package net.frozenorb.foxtrot.command.commands.subcommands.teamsubcommands;
+
+import net.frozenorb.foxtrot.FoxtrotPlugin;
+import net.frozenorb.foxtrot.command.annotations.Command;
+import net.frozenorb.foxtrot.factionactiontracker.FactionActionTracker;
+import net.frozenorb.foxtrot.team.Team;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by chasechocolate.
+ */
+public class TeamDisbandCommand {
+
+    @Command(names={ "team disband", "t disband", "f disband", "faction disband", "fac disband" }, permissionNode="")
+    public static void teamDisband(Player player) {
+        Team team = FoxtrotPlugin.getInstance().getTeamHandler().getPlayerTeam(player.getName());
+
+        if (team == null){
+            player.sendMessage(ChatColor.RED + "You are not on a team!");
+            return;
+        }
+
+        if (!team.isOwner(player.getName())) {
+            player.sendMessage(ChatColor.RED + "You must be the leader of the team to disband it!");
+            return;
+        }
+
+        if (team.isRaidable()) {
+            player.sendMessage(ChatColor.RED + "You cannot disband your team while raidable.");
+            return;
+        }
+
+        for (Player online : team.getOnlineMembers()) {
+            online.sendMessage(ChatColor.RED.toString() + ChatColor.BOLD + player.getName() + " has disbanded the team.");
+        }
+
+        FactionActionTracker.logAction(team, "actions", "Faction disbanded. [Disbanded by: " + player.getName() + "]");
+        FoxtrotPlugin.getInstance().getTeamHandler().removeTeam(team.getName());
+    }
+
+}
