321853a81346c95ef5016fd082f18209e2aa9a43
macguy8 <macfinder1@me.com>
2014-11-23 08:15:07 -0500

    Add /pvp addlives.

---
+++ src/main/java/net/frozenorb/foxtrot/command/commands/pvp/PvPAddLivesCommand.java
@@ -0,0 +1,31 @@
+package net.frozenorb.foxtrot.command.commands.pvp;
+
+import net.frozenorb.foxtrot.FoxtrotPlugin;
+import net.frozenorb.foxtrot.command.annotations.Command;
+import net.frozenorb.foxtrot.command.annotations.Param;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by macguy8 on 11/23/2014.
+ */
+public class PvPAddLivesCommand {
+
+    @Command(names={ "pvptimer addlives", "timer addlives", "pvp addlives", "pvptimer addlives", "timer addlives", "pvp addlives" }, permissionNode="op")
+    public static void pvpSetLives(Player sender, @Param(name="player") OfflinePlayer target, @Param(name="Life Type") String lifeType, @Param(name="Amount") int amount) {
+        if (lifeType.equalsIgnoreCase("soulbound")) {
+            FoxtrotPlugin.getInstance().getSoulboundLivesMap().setLives(target.getName(), FoxtrotPlugin.getInstance().getSoulboundLivesMap().getLives(target.getName()) + amount);
+            sender.sendMessage(ChatColor.YELLOW + "Gave " + ChatColor.GREEN + target.getName() + ChatColor.YELLOW + " " + amount + " soulbound lives.");
+        } else if (lifeType.equalsIgnoreCase("friend")) {
+            FoxtrotPlugin.getInstance().getFriendLivesMap().setLives(target.getName(), FoxtrotPlugin.getInstance().getFriendLivesMap().getLives(target.getName()) + amount);
+            sender.sendMessage(ChatColor.YELLOW + "Gave " + ChatColor.GREEN + target.getName() + ChatColor.YELLOW + " " + amount + " friend lives.");
+        } else if (lifeType.equalsIgnoreCase("transferable")) {
+            FoxtrotPlugin.getInstance().getTransferableLivesMap().setLives(target.getName(), FoxtrotPlugin.getInstance().getTransferableLivesMap().getLives(target.getName()) + amount);
+            sender.sendMessage(ChatColor.YELLOW + "Gave " + ChatColor.GREEN + target.getName() + ChatColor.YELLOW + " " + amount + " transferable lives.");
+        } else {
+            sender.sendMessage(ChatColor.RED + "Not a valid life type: Options are soulbound, friend, or transferable.");
+        }
+    }
+
+}
