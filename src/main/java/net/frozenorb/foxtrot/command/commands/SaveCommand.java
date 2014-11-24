501b0597e43664ad7a9294a855bbb052d6de8edf
macguy8 <macfinder1@me.com>
2014-11-23 21:05:37 -0500

    Allow /saveredis from Console.

--- src/main/java/net/frozenorb/foxtrot/command/commands/SaveCommand.java
+++ src/main/java/net/frozenorb/foxtrot/command/commands/SaveCommand.java
@@ -2,17 +2,18 @@ package net.frozenorb.foxtrot.command.commands;
 
 import net.frozenorb.foxtrot.command.annotations.Command;
 import net.frozenorb.foxtrot.jedis.RedisSaveTask;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class SaveCommand {
 
     @Command(names={ "Save", "SaveRedis" }, permissionNode="op")
-    public static void save(Player sender) {
+    public static void save(CommandSender sender) {
         RedisSaveTask.save(false);
     }
 
     @Command(names={ "Save ForceAll", "SaveRedis ForceAll" }, permissionNode="op")
-    public static void saveForceAll(Player sender) {
+    public static void saveForceAll(CommandSender sender) {
         RedisSaveTask.save(true);
     }
 
