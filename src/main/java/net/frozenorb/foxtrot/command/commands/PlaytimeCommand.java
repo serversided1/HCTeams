f919c2f7f0c7f88d742e1b78dcf5e92d729a1898
macguy8 <macfinder1@me.com>
2014-12-03 12:11:05 -0500

    Add /playtime to see the playtime of yourself and other players.

---
+++ src/main/java/net/frozenorb/foxtrot/command/commands/PlaytimeCommand.java
@@ -0,0 +1,29 @@
+package net.frozenorb.foxtrot.command.commands;
+
+import net.frozenorb.foxtrot.FoxtrotPlugin;
+import net.frozenorb.foxtrot.command.annotations.Command;
+import net.frozenorb.foxtrot.command.annotations.Param;
+import net.frozenorb.foxtrot.jedis.persist.PlaytimeMap;
+import net.frozenorb.foxtrot.util.TimeUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by macguy8 on 12/3/2014.
+ */
+public class PlaytimeCommand {
+
+    @Command(names={ "Playtime", "PTime" }, permissionNode="")
+    public static void playtime(Player sender, @Param(name="Target", defaultValue="self") OfflinePlayer target) {
+        PlaytimeMap playtime = FoxtrotPlugin.getInstance().getPlaytimeMap();
+        long playtimeTime = playtime.getPlaytime(target.getName());
+
+        if (target.isOnline() && sender.canSee(target.getPlayer())) {
+            playtimeTime += playtime.getCurrentSession(target.getName()) / 1000L;
+        }
+
+        sender.sendMessage(ChatColor.GREEN + target.getName() + ChatColor.YELLOW + "'s total playtime is " + ChatColor.GREEN + TimeUtils.getDurationBreakdown(playtimeTime * 1000L) + ChatColor.YELLOW + ".");
+    }
+
+}
