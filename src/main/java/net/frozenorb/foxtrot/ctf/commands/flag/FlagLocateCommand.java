6c1fbcd11353496b2f05e28aa1f905e0d97bb3da
Colin McDonald <macguy8.main@gmail.com>
2014-12-28 00:53:54 -0500

    Finish /flag locate.

--- src/main/java/net/frozenorb/foxtrot/ctf/commands/flag/FlagLocateCommand.java
+++ src/main/java/net/frozenorb/foxtrot/ctf/commands/flag/FlagLocateCommand.java
@@ -4,6 +4,7 @@ import net.frozenorb.foxtrot.FoxtrotPlugin;
 import net.frozenorb.foxtrot.command.annotations.Command;
 import net.frozenorb.foxtrot.command.annotations.Param;
 import net.frozenorb.foxtrot.ctf.CTFHandler;
+import net.frozenorb.foxtrot.ctf.enums.CTFFlagState;
 import net.frozenorb.foxtrot.ctf.game.CTFFlag;
 import net.frozenorb.foxtrot.ctf.game.CTFGame;
 import org.bukkit.ChatColor;
@@ -22,6 +23,21 @@ public class FlagLocateCommand {
 
         for (CTFFlag flag : game.getFlags().values()) {
             if (flag.getColor().getName().equalsIgnoreCase(flagColor)) {
+                sender.sendMessage(flag.getColor().getChatColor() + flag.getColor().getName() + " Flag:");
+
+                String locationString;
+
+                if (flag.getState() == CTFFlagState.CAP_POINT) {
+                    locationString = "At cap point";
+                } else {
+                    locationString = "Held by " + flag.getFlagHolder().getName();
+                }
+
+                sender.sendMessage(ChatColor.YELLOW + "Location: " + ChatColor.WHITE + locationString);
+                sender.sendMessage(ChatColor.YELLOW + "Return location: " + ChatColor.WHITE + flag.getCaptureLocation().getBlockX() + ", " + flag.getCaptureLocation().getBlockY() + ", " + flag.getCaptureLocation().getBlockZ());
+                sender.sendMessage(ChatColor.YELLOW + "Spawn location: " + ChatColor.WHITE + flag.getSpawnLocation().getBlockX() + ", " + flag.getSpawnLocation().getBlockY() + ", " + flag.getSpawnLocation().getBlockZ());
+                sender.sendMessage(ChatColor.YELLOW + "Current location: " + ChatColor.WHITE + flag.getLocation().getBlockX() + ", " + flag.getLocation().getBlockY() + ", " + flag.getLocation().getBlockZ());
+
                 return;
             }
         }
