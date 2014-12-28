4e5c1837a87fd16233824f5fdd7c90fb427e1d63
Colin McDonald <macguy8.main@gmail.com>
2014-12-27 22:10:03 -0500

    Remove unneeded backup system.

--- src/main/java/net/frozenorb/foxtrot/command/commands/SaveCommand.java
+++ src/main/java/net/frozenorb/foxtrot/command/commands/SaveCommand.java
@@ -2,39 +2,16 @@ package net.frozenorb.foxtrot.command.commands;
 
 import net.frozenorb.foxtrot.command.annotations.Command;
 import net.frozenorb.foxtrot.jedis.RedisSaveTask;
-import net.frozenorb.foxtrot.util.BackupUtils;
-import net.frozenorb.foxtrot.util.FoxCallback;
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class SaveCommand {
 
-    @Command(names={ "Save" }, permissionNode="op")
-    public static void save(CommandSender sender) {
-        sender.sendMessage(ChatColor.RED + "/saveredis - saves changes to redis");
-        sender.sendMessage(ChatColor.RED + "/saveserver - [alpha] takes a backup of the server directory");
-    }
-
-    @Command(names={ "SaveRedis" }, permissionNode="op")
+    @Command(names={ "SaveRedis", "Save" }, permissionNode="op")
     public static void saveRedis(CommandSender sender) {
         RedisSaveTask.save(false);
     }
 
-    @Command(names={ "SaveServer" }, permissionNode="op")
-    public static void saveServer(CommandSender sender) {
-        sender.sendMessage(ChatColor.YELLOW + "Server save and backup started.");
-
-        BackupUtils.fullBackup(new FoxCallback() {
-
-            @Override
-            public void call(Object object) {
-                sender.sendMessage(ChatColor.YELLOW + "Server save and backup completed.");
-            }
-
-        });
-    }
-
-    @Command(names={ "SaveRedis ForceAll" }, permissionNode="op")
+    @Command(names={ "SaveRedis ForceAll", "Save ForceAll" }, permissionNode="op")
     public static void saveRedisForceAll(CommandSender sender) {
         RedisSaveTask.save(true);
     }
