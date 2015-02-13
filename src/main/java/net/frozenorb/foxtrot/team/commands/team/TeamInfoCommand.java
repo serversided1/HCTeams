68159e6cf9fef86e59257da1e2f499f60ca81dee
Colin McDonald <macguy8.main@gmail.com>
2015-02-13 16:13:28 -0500

    Send both teams in the case we get a match while /t who'ing

--- src/main/java/net/frozenorb/foxtrot/team/commands/team/TeamInfoCommand.java
+++ src/main/java/net/frozenorb/foxtrot/team/commands/team/TeamInfoCommand.java
@@ -1,5 +1,6 @@
 package net.frozenorb.foxtrot.team.commands.team;
 
+import net.frozenorb.foxtrot.FoxtrotPlugin;
 import net.frozenorb.foxtrot.command.annotations.Command;
 import net.frozenorb.foxtrot.command.annotations.Param;
 import net.frozenorb.foxtrot.team.Team;
@@ -9,6 +10,12 @@ public class TeamInfoCommand {
 
     @Command(names={ "team info", "t info", "f info", "faction info", "fac info", "team who", "t who", "f who", "faction who", "fac who", "team show", "t show", "f show", "faction show", "fac show", "team i", "t i", "f i", "faction i", "fac i" }, permissionNode="")
     public static void teamInfo(Player sender, @Param(name="team", defaultValue="self") Team target) {
+        Team exactPlayerTeam = FoxtrotPlugin.getInstance().getTeamHandler().getPlayerTeam(target.getName());
+
+        if (exactPlayerTeam != null && exactPlayerTeam != target) {
+            exactPlayerTeam.sendTeamInfo(sender);
+        }
+
         target.sendTeamInfo(sender);
     }
 
