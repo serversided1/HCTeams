ccdedc2a21e47af2f631046c7b366b5c48097da9
itsjhalt <jhalterman@minehq.com>
2015-02-23 11:08:40 -0800

    Add a few more commands to the /team help page.

--- src/main/java/net/frozenorb/foxtrot/team/commands/team/TeamCommand.java
+++ src/main/java/net/frozenorb/foxtrot/team/commands/team/TeamCommand.java
@@ -12,7 +12,7 @@ public class TeamCommand {
         String[] msg = {
 
                 "§6§m-----------------------------------------------------",
-                "§d§lTeam Help §7- §fTeam Help",
+                "§9§lTeam Help §7- §eTeam Help",
                 "§7§m-----------------------------------------------------",
 
 
@@ -21,6 +21,7 @@ public class TeamCommand {
                 "§e/t accept <teamName> §7- Accept a pending invitation",
                 "§e/t leave §7- Leave your current team",
                 "§e/t home §7- Teleport to your team home",
+                "§e/t stuck §7- Teleport out of enemy territory",
                 "§e/t deposit <amount§7|§eall> §7- Deposit money into your team balance",
 
 
@@ -28,6 +29,7 @@ public class TeamCommand {
                 "§9Information Commands:",
                 "§e/t who [player§7|§eteamName] §7- Display team information",
                 "§e/t map §7- Show nearby claims (identified by pillars)",
+                "§e/t list §7- Show list of teams online (sorted by most online)",
 
 
                 "",
@@ -36,6 +38,8 @@ public class TeamCommand {
                 "§e/t uninvite <player> §7- Revoke an invitation",
                 "§e/t invites §7- List all open invitations",
                 "§e/t kick <player> §7- Kick a player from your team.",
+                "§e/t claim §7- Start a claim for your team.",
+                "§e/t subclaim §7- Show the subclaim help page.",
                 "§e/t sethome §7- Set your team's home at your current location",
                 "§e/t withdraw <amount> §7- Withdraw money from your team's balance",
                 "§e/t motd [message here] §7- Set your team's message of the day",
@@ -46,6 +50,7 @@ public class TeamCommand {
                 "§e/t promote <player> §7- Promote a member to Captain",
                 "§e/t demote <player> §7- Demotes a Captain to member",
                 "§e/t unclaim [all] §7- Unclaim land",
+                "§e/t rename <newName> §7- Rename your team.",
                 "§e/t disband §7- Disband your team.",
 
 
