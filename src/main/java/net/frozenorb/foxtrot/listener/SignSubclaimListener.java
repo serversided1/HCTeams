9bc2a39da0a064f8053513314a8daa15799d6cbc
itsjhalt <jhalterman@minehq.com>
2015-02-22 22:10:27 -0800

    Rookie mistake ;(

--- src/main/java/net/frozenorb/foxtrot/listener/SignSubclaimListener.java
+++ src/main/java/net/frozenorb/foxtrot/listener/SignSubclaimListener.java
@@ -74,13 +74,12 @@ public class SignSubclaimListener implements Listener {
         }
 
         if (!found && !(playerTeam.isOwner(event.getPlayer().getName()) || playerTeam.isCaptain(event.getPlayer().getName()))) {
-            event.getPlayer().sendMessage(ChatColor.YELLOW + "It appears you've forgotten to add yourself to this subclaim! You're not an officer, so this would lock you out of your subclaim.");
-            event.getPlayer().sendMessage(ChatColor.YELLOW + "We've automatically added you to your subclaim.");
-
             if (event.getPlayer().getName().length() > 15) {
                 event.getPlayer().sendMessage("§cYour name is too long for sign subclaims. Consider changing your username.");
                 return;
             }
+            event.getPlayer().sendMessage(ChatColor.YELLOW + "It appears you've forgotten to add yourself to this subclaim! You're not an officer, so this would lock you out of your subclaim.");
+            event.getPlayer().sendMessage(ChatColor.YELLOW + "We've automatically added you to your subclaim.");
             event.setLine(1, event.getPlayer().getName());
         }
 
