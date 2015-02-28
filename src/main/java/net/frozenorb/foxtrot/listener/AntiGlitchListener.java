09de04cd0b04e9862c089b165d1b59dcc06dcc3a
Colin McDonald <macguy8.main@gmail.com>
2015-02-27 23:55:06 -0500

    Fix HCT-5009 Disable piston-pushing rails. This can lead to duping powered rails.

--- src/main/java/net/frozenorb/foxtrot/listener/AntiGlitchListener.java
+++ src/main/java/net/frozenorb/foxtrot/listener/AntiGlitchListener.java
@@ -1,17 +1,36 @@
 package net.frozenorb.foxtrot.listener;
 
 import net.frozenorb.foxtrot.team.claims.LandBoard;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.block.BlockPistonRetractEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 public class AntiGlitchListener implements Listener {
 
-    @EventHandler(priority= EventPriority.MONITOR)
+    @EventHandler(priority=EventPriority.MONITOR)
     public void onVerticalBlockPlaceGlitch(BlockPlaceEvent event) {
         if (LandBoard.getInstance().getTeam(event.getBlock().getLocation()) != null && event.isCancelled()) {
             event.getPlayer().teleport(event.getPlayer().getLocation());
+        }
+    }
+
+    @EventHandler
+    public void onBlockPistonRetract(BlockPistonRetractEvent event) {
+        if (event.getBlock().getType().name().contains("RAIL")) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onBlockPistonExtend(BlockPistonExtendEvent event) {
+        for (Block block : event.getBlocks()) {
+            if (block.getType().name().contains("RAIL")) {
+                event.setCancelled(true);
+            }
         }
     }
 
