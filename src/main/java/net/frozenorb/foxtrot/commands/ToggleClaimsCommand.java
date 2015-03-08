e99edfbe1152931d177019c5912e21c1a9596602
Colin McDonald <macguy8.main@gmail.com>
2015-03-07 19:29:11 -0500

    Add claim toggling feature. @jhalt

---
+++ src/main/java/net/frozenorb/foxtrot/commands/ToggleClaimsCommand.java
@@ -0,0 +1,16 @@
+package net.frozenorb.foxtrot.commands;
+
+import net.frozenorb.foxtrot.team.claims.LandBoard;
+import net.frozenorb.qlib.command.Command;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class ToggleClaimsCommand {
+
+    @Command(names={ "ToggleClaims" }, permissionNode="op")
+    public static void toggleClaims(Player sender) {
+        LandBoard.getInstance().setClaimsEnabled(!LandBoard.getInstance().isClaimsEnabled());
+        sender.sendMessage(ChatColor.YELLOW + "Claims enabled? " + ChatColor.LIGHT_PURPLE + (LandBoard.getInstance().isClaimsEnabled() ? "Yes" : "No"));
+    }
+
+}
