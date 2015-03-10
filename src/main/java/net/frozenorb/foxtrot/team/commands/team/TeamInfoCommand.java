602706f4cd3c4002c6772c20d91e27966644e80a
Colin McDonald <macguy8.main@gmail.com>
2015-03-09 20:04:13 -0400

    Fix lag.

--- src/main/java/net/frozenorb/foxtrot/team/commands/team/TeamInfoCommand.java
+++ src/main/java/net/frozenorb/foxtrot/team/commands/team/TeamInfoCommand.java
@@ -5,18 +5,25 @@ import net.frozenorb.foxtrot.team.Team;
 import net.frozenorb.qlib.command.Command;
 import net.frozenorb.qlib.command.Parameter;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class TeamInfoCommand {
 
     @Command(names={ "team info", "t info", "f info", "faction info", "fac info", "team who", "t who", "f who", "faction who", "fac who", "team show", "t show", "f show", "faction show", "fac show", "team i", "t i", "f i", "faction i", "fac i" }, permissionNode="")
     public static void teamInfo(Player sender, @Parameter(name="team", defaultValue="self") Team target) {
-        Team exactPlayerTeam = FoxtrotPlugin.getInstance().getTeamHandler().getTeam(target.getName());
+        new BukkitRunnable() {
 
-        if (exactPlayerTeam != null && exactPlayerTeam != target) {
-            exactPlayerTeam.sendTeamInfo(sender);
-        }
+            public void run() {
+                Team exactPlayerTeam = FoxtrotPlugin.getInstance().getTeamHandler().getTeam(target.getName());
 
-        target.sendTeamInfo(sender);
+                if (exactPlayerTeam != null && exactPlayerTeam != target) {
+                    exactPlayerTeam.sendTeamInfo(sender);
+                }
+
+                target.sendTeamInfo(sender);
+            }
+
+        }.runTaskAsynchronously(FoxtrotPlugin.getInstance());
     }
 
 }
