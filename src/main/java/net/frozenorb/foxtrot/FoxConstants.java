ebddede4c8b2facc329744926054dc554cd3e75f
Colin McDonald <macguy8.main@gmail.com>
2015-03-12 16:12:01 -0400

    Add weird Kerem code.

--- src/main/java/net/frozenorb/foxtrot/FoxConstants.java
+++ src/main/java/net/frozenorb/foxtrot/FoxConstants.java
@@ -6,7 +6,12 @@ import net.frozenorb.foxtrot.util.ClickableUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class FoxConstants {
+
+    private static final Pattern URL_PATTERN = Pattern.compile("(" + String.valueOf(org.bukkit.ChatColor.COLOR_CHAR) + "[0-9a-fk-or])|(\\n)|((?:(?:https?)://)?(?:[-\\w_\\.]{2,}\\.[a-z]{2,4}.*?(?=[\\.\\?!,;:]?(?:[" + String.valueOf(org.bukkit.ChatColor.COLOR_CHAR) + " \\n]|$))))", Pattern.CASE_INSENSITIVE);
 
     public static String teamChatFormat(Player player, String message) {
         return (ChatColor.DARK_AQUA + "(Team) " + player.getName() + ": " + ChatColor.YELLOW + message);
@@ -39,7 +44,43 @@ public final class FoxConstants {
 
         chat.text(ChatColor.GOLD + "]" + highRollerString + customPrefixString).then();
         ClickableUtils.appendClickableName(player.getDisplayName(), chat);
-        chat.text(ChatColor.WHITE + ": " + message);
+        chat.text(ChatColor.WHITE + ": ");
+
+        Matcher matcher = URL_PATTERN.matcher(message);
+        String match = null;
+        int currentIndex = 0;
+
+        while (matcher.find()) {
+            int groupId = 0;
+
+            while ((match = matcher.group(++groupId)) == null) {
+                // NOOP
+            }
+
+            int index = (matcher.start(groupId));
+            String part = message.substring(currentIndex, index);
+            currentIndex = index;
+            chat.then(part);
+
+            switch (groupId) {
+                case 3:
+                    if (!(match.startsWith("http://") || match.startsWith("https://"))) {
+                        match = "http://" + match;
+                    }
+
+                    int newIndex = matcher.end(groupId);
+                    String tp = message.substring(currentIndex, newIndex);
+                    currentIndex = newIndex;
+                    chat.then(tp);
+                    chat.link(match).tooltip("§aOpen webpage!");
+            }
+
+            currentIndex = matcher.end(groupId);
+        }
+
+        if (currentIndex < message.length()) {
+            chat.then(message);
+        }
 
         return (chat);
     }
