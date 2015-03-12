aa97f1577aa22ca6aa42baece29efe173017bef0
Colin McDonald <macguy8.main@gmail.com>
2015-03-12 16:05:10 -0400

    Remove unused ClickableUtils methods.

--- src/main/java/net/frozenorb/foxtrot/util/ClickableUtils.java
+++ src/main/java/net/frozenorb/foxtrot/util/ClickableUtils.java
@@ -7,20 +7,8 @@ public class ClickableUtils {
 
     private ClickableUtils() {}
 
-    public static FancyMessage clickableName(String player) {
-        FancyMessage appendTo = new FancyMessage();
-        appendClickableName(player, appendTo);
-        return (appendTo);
-    }
-
     public static void appendClickableName(String player, FancyMessage appendTo) {
         appendTo.text(player).link("http://www.hcteams.com/player/" + ChatColor.stripColor(player)).tooltip(ChatColor.GREEN + "Click to view " + player + ChatColor.GREEN + "'s profile").then();
-    }
-
-    public static FancyMessage clickableTeam(String team) {
-        FancyMessage appendTo = new FancyMessage();
-        appendClickableTeam(team, appendTo);
-        return (appendTo);
     }
 
     public static void appendClickableTeam(String team, FancyMessage appendTo) {
