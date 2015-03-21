d7e7969b457b1f44404eb73a6bc8fe123d685995
Colin McDonald <macguy8.main@gmail.com>
2015-03-20 20:50:06 -0400

    Add /setteambalance command for jhalt!

---
+++ src/main/java/net/frozenorb/foxtrot/team/commands/SetTeamBalanceCommand.java
@@ -0,0 +1,17 @@
+package net.frozenorb.foxtrot.team.commands;
+
+import net.frozenorb.foxtrot.team.Team;
+import net.frozenorb.qlib.command.Command;
+import net.frozenorb.qlib.command.Parameter;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class SetTeamBalanceCommand {
+
+    @Command(names={ "setteambalance" }, permissionNode="foxtrot.forceleader")
+    public static void setTeamBalance(Player sender, @Parameter(name="Team") Team team,  @Parameter(name="balance") float balance) {
+        team.setBalance(balance);
+        sender.sendMessage(ChatColor.LIGHT_PURPLE + team.getName() + ChatColor.YELLOW + "'s balance is now " + ChatColor.LIGHT_PURPLE + team.getBalance() + ChatColor.YELLOW + ".");
+    }
+
+}
