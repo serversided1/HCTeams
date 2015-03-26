fd597ab56177ee6117cc35ee69dceff49feb5970
Colin McDonald <macguy8.main@gmail.com>
2015-03-26 16:40:17 -0400

    Cleanup Bard code a bit.

--- src/main/java/net/frozenorb/foxtrot/pvpclasses/pvpclasses/BardClass.java
+++ src/main/java/net/frozenorb/foxtrot/pvpclasses/pvpclasses/BardClass.java
@@ -1,20 +1,49 @@
 package net.frozenorb.foxtrot.pvpclasses.pvpclasses;
 
+import lombok.Getter;
+import net.frozenorb.foxtrot.Foxtrot;
+import net.frozenorb.foxtrot.listener.FoxListener;
+import net.frozenorb.foxtrot.pvpclasses.PvPClass;
+import net.frozenorb.foxtrot.pvpclasses.PvPClassHandler;
 import net.frozenorb.foxtrot.pvpclasses.pvpclasses.bard.BardEffect;
+import net.frozenorb.foxtrot.server.SpawnTagHandler;
+import net.frozenorb.foxtrot.team.Team;
 import net.frozenorb.foxtrot.team.dtr.DTRBitmask;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
-public class BardClass extends BaseBardClass implements Listener {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class BardClass extends PvPClass implements Listener {
+
+    public final Map<Material, BardEffect> BARD_CLICK_EFFECTS = new HashMap<>();
+    public final Map<Material, BardEffect> BARD_PASSIVE_EFFECTS = new HashMap<>();
+
+    @Getter private static Map<String, Long> lastEffectUsage = new ConcurrentHashMap<>();
+    @Getter private static Map<String, Float> energy = new ConcurrentHashMap<>();
+
+    public static final int BARD_RANGE = 20;
+    public static final int EFFECT_COOLDOWN = 10 * 1000;
+    public static final float MAX_ENERGY = 100;
+    public static final float ENERGY_REGEN_PER_SECOND = 1;
 
     public BardClass() {
-        super("Bard", "GOLD_");
+        super("Bard", 15, "GOLD_", null);
 
         // Click buffs
         BARD_CLICK_EFFECTS.put(Material.BLAZE_POWDER, new BardEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 20 * 5, 1), 45));
@@ -34,6 +63,34 @@ public class BardClass extends BaseBardClass implements Listener {
         BARD_PASSIVE_EFFECTS.put(Material.IRON_INGOT, new BardEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 20 * 6, 0)));
         BARD_PASSIVE_EFFECTS.put(Material.GHAST_TEAR, new BardEffect(new PotionEffect(PotionEffectType.REGENERATION, 20 * 6, 0)));
         BARD_PASSIVE_EFFECTS.put(Material.MAGMA_CREAM, new BardEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 20 * 6, 0)));
+
+        new BukkitRunnable() {
+
+            public void run() {
+                for (Player player : Foxtrot.getInstance().getServer().getOnlinePlayers()) {
+                    if (!PvPClassHandler.hasKitOn(player, BardClass.this) || Foxtrot.getInstance().getPvPTimerMap().hasTimer(player.getUniqueId())) {
+                        continue;
+                    }
+
+                    if (energy.containsKey(player.getName())) {
+                        if (energy.get(player.getName()) == MAX_ENERGY) {
+                            continue;
+                        }
+
+                        energy.put(player.getName(), Math.min(MAX_ENERGY, energy.get(player.getName()) + ENERGY_REGEN_PER_SECOND));
+                    } else {
+                        energy.put(player.getName(), 0F);
+                    }
+
+                    int manaInt = energy.get(player.getName()).intValue();
+
+                    if (manaInt % 10 == 0) {
+                        player.sendMessage(ChatColor.AQUA + "Bard Energy: " + ChatColor.GREEN + manaInt);
+                    }
+                }
+            }
+
+        }.runTaskTimer(Foxtrot.getInstance(), 15L, 20L);
     }
 
     @Override
@@ -41,6 +98,10 @@ public class BardClass extends BaseBardClass implements Listener {
         player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1));
         player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, Integer.MAX_VALUE, 1));
         player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, Integer.MAX_VALUE, 0));
+
+        if (Foxtrot.getInstance().getPvPTimerMap().hasTimer(player.getUniqueId())) {
+            player.sendMessage(ChatColor.RED + "You are in PvP Protection and cannot use Bard effects. Type '/pvp enable' to remove your protection.");
+        }
     }
 
     @Override
@@ -62,9 +123,116 @@ public class BardClass extends BaseBardClass implements Listener {
         }
     }
 
+
+    @Override
+    public void remove(Player player) {
+        energy.remove(player.getName());
+    }
+
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        super.onPlayerInteract(event);
+        if (!event.getAction().name().contains("RIGHT_") || !event.hasItem() || !BARD_CLICK_EFFECTS.containsKey(event.getItem().getType()) || !PvPClassHandler.hasKitOn(event.getPlayer(), this)) {
+            return;
+        }
+
+        if (DTRBitmask.SAFE_ZONE.appliesAt(event.getPlayer().getLocation())) {
+            event.getPlayer().sendMessage(ChatColor.RED + "Bard effects cannot be used while in spawn.");
+            return;
+        }
+
+        if (Foxtrot.getInstance().getPvPTimerMap().hasTimer(event.getPlayer().getUniqueId())) {
+            event.getPlayer().sendMessage(ChatColor.RED + "You are in PvP Protection and cannot use Bard effects. Type '/pvp enable' to remove your protection.");
+            return;
+        }
+
+        if (getLastEffectUsage().containsKey(event.getPlayer().getName()) && getLastEffectUsage().get(event.getPlayer().getName()) > System.currentTimeMillis() && event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+            long millisLeft = getLastEffectUsage().get(event.getPlayer().getName()) - System.currentTimeMillis();
+
+            double value = (millisLeft / 1000D);
+            double sec = Math.round(10.0 * value) / 10.0;
+
+            event.getPlayer().sendMessage(ChatColor.RED + "You cannot use this for another " + ChatColor.BOLD + sec + ChatColor.RED + " seconds!");
+            return;
+        }
+
+        BardEffect bardEffect = BARD_CLICK_EFFECTS.get(event.getItem().getType());
+
+        if (bardEffect.getEnergy() > energy.get(event.getPlayer().getName())) {
+            event.getPlayer().sendMessage(ChatColor.RED + "You do not have enough energy for this! You need " + bardEffect.getEnergy() + " energy, but you only have " + energy.get(event.getPlayer().getName()).intValue());
+            return;
+        }
+
+        energy.put(event.getPlayer().getName(), energy.get(event.getPlayer().getName()) - bardEffect.getEnergy());
+
+        boolean negative = bardEffect.getPotionEffect() != null && FoxListener.DEBUFFS.contains(bardEffect.getPotionEffect().getType());
+
+        getLastEffectUsage().put(event.getPlayer().getName(), System.currentTimeMillis() + EFFECT_COOLDOWN);
+        SpawnTagHandler.addSeconds(event.getPlayer(), negative ? 30 : 30);
+        giveBardEffect(event.getPlayer(), bardEffect, !negative, true);
+
+        if (event.getPlayer().getItemInHand().getAmount() == 1) {
+            event.getPlayer().setItemInHand(new ItemStack(Material.AIR));
+            event.getPlayer().updateInventory();
+        } else {
+            event.getPlayer().getItemInHand().setAmount(event.getPlayer().getItemInHand().getAmount() - 1);
+        }
+    }
+
+    public void giveBardEffect(Player source, BardEffect bardEffect, boolean friendly, boolean persistOldValues) {
+        for (Player player : getNearbyPlayers(source, friendly)) {
+            if (DTRBitmask.SAFE_ZONE.appliesAt(player.getLocation())) {
+                continue;
+            }
+
+            // Bards can't get Strength.
+            // Yes, that does need to use .equals. PotionEffectType is NOT an enum.
+            if (PvPClassHandler.hasKitOn(player, this) && bardEffect.getPotionEffect() != null && bardEffect.getPotionEffect().getType().equals(PotionEffectType.INCREASE_DAMAGE)) {
+                continue;
+            }
+
+            if (bardEffect.getPotionEffect() != null) {
+                smartAddPotion(player, bardEffect.getPotionEffect(), persistOldValues);
+            } else {
+                Material material = source.getItemInHand().getType();
+                giveCustomBardEffect(player, material);
+            }
+        }
+    }
+
+    public void giveCustomBardEffect(Player player, Material material) {
+        // For now this does nothing.
+    }
+
+    public List<Player> getNearbyPlayers(Player player, boolean friendly) {
+        List<Player> valid = new ArrayList<>();
+        Team sourceTeam = Foxtrot.getInstance().getTeamHandler().getTeam(player);
+
+        // We divide by 2 so that the range isn't as much on the Y level (and can't be abused by standing on top of / under events)
+        for (Entity entity : player.getNearbyEntities(BARD_RANGE, BARD_RANGE / 2, BARD_RANGE)) {
+            if (entity instanceof Player) {
+                Player nearbyPlayer = (Player) entity;
+
+                if (sourceTeam == null) {
+                    if (!friendly) {
+                        valid.add(nearbyPlayer);
+                    }
+
+                    continue;
+                }
+
+                boolean isFriendly = sourceTeam.isMember(nearbyPlayer.getUniqueId());
+                boolean isAlly = sourceTeam.isAlly(nearbyPlayer.getUniqueId());
+
+                if (friendly && isFriendly) {
+                    valid.add(nearbyPlayer);
+                } else if (!friendly && !isFriendly && !isAlly) { // the isAlly is here so you can't give your allies negative effects, but so you also can't give them positive effects.
+                    valid.add(nearbyPlayer);
+                }
+            }
+        }
+
+        valid.add(player);
+        return (valid);
     }
 
 }
