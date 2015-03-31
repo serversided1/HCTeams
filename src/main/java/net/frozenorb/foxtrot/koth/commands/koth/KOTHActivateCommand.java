59d9fdb2c9850914e0ae30f5bb36534ca4f46b82
Colin McDonald <macguy8.main@gmail.com>
2015-03-31 16:03:15 -0400

    Don't let more than 1 KOTH be active at once.

--- src/main/java/net/frozenorb/foxtrot/koth/commands/koth/KOTHActivateCommand.java
+++ src/main/java/net/frozenorb/foxtrot/koth/commands/koth/KOTHActivateCommand.java
@@ -1,5 +1,6 @@
 package net.frozenorb.foxtrot.koth.commands.koth;
 
+import net.frozenorb.foxtrot.Foxtrot;
 import net.frozenorb.foxtrot.koth.KOTH;
 import net.frozenorb.qlib.command.Command;
 import net.frozenorb.qlib.command.Parameter;
@@ -10,6 +11,13 @@ public class KOTHActivateCommand {
 
     @Command(names={ "KOTH Activate", "KOTH Active" }, permissionNode="foxtrot.koth")
     public static void kothActivate(Player sender, @Parameter(name="KOTH") KOTH target) {
+        for (KOTH koth : Foxtrot.getInstance().getKOTHHandler().getKOTHs()) {
+            if (koth.isActive() && !koth.isHidden()) {
+                sender.sendMessage(ChatColor.RED + "Another KOTH (" + koth.getName() + ") is already active.");
+                return;
+            }
+        }
+        
         target.activate();
         sender.sendMessage(ChatColor.GRAY + "Activated " + target.getName() + ".");
     }
