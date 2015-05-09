029f77bdc1615775dfa33281c796b7c9f153e7ac
Colin McDonald <macguy8.main@gmail.com>
2015-05-09 14:43:19 -0400

    Make /KOTH Level op-only 'cause #MineHQStaffOP

--- src/main/java/net/frozenorb/foxtrot/koth/commands/koth/KOTHLevelCommand.java
+++ src/main/java/net/frozenorb/foxtrot/koth/commands/koth/KOTHLevelCommand.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 
 public class KOTHLevelCommand {
 
-    @Command(names={ "KOTH Level" }, permissionNode="foxtrot.koth")
+    @Command(names={ "KOTH Level" }, permissionNode="op")
     public static void kothLevel(Player sender, @Parameter(name="koth") KOTH koth, @Parameter(name="level") int level) {
         koth.setLevel(level);
         sender.sendMessage(ChatColor.GRAY + "Set level for the " + koth.getName() + " KOTH.");
