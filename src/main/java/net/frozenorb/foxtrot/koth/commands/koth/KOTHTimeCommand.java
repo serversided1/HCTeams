d09de13e79ac9ca4b1a115d99da59a12d3cd01b1
Colin McDonald <macguy8.main@gmail.com>
2015-05-10 19:41:37 -0400

    Change /KOTH Time to use minutes instead of seconds.

--- src/main/java/net/frozenorb/foxtrot/koth/commands/koth/KOTHTimeCommand.java
+++ src/main/java/net/frozenorb/foxtrot/koth/commands/koth/KOTHTimeCommand.java
@@ -9,8 +9,12 @@ import org.bukkit.entity.Player;
 public class KOTHTimeCommand {
 
     @Command(names={ "KOTH Time" }, permissionNode="foxtrot.koth.admin")
-    public static void kothTime(Player sender, @Parameter(name="koth") KOTH koth, @Parameter(name="time") int time) {
-        koth.setCapTime(time);
+    public static void kothTime(Player sender, @Parameter(name="koth") KOTH koth, @Parameter(name="time") float time) {
+        if (time > 20F) {
+            sender.sendMessage(ChatColor.RED.toString() + ChatColor.BOLD + "This command was changed! The time parameter is now in minutes, not seconds. For example, to set a KOTH's capture time to 20 minutes 30 seconds, use /koth time 20.5");
+        }
+
+        koth.setCapTime((int) (time * 60F));
         sender.sendMessage(ChatColor.GRAY + "Set cap time for the " + koth.getName() + " KOTH.");
     }
 
