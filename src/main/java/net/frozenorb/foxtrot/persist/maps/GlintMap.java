34211a4160700279464c919074e83e70b88ea03d
Colin McDonald <macguy8.main@gmail.com>
2015-05-23 03:59:26 -0400

    Remove old /glint code.

--- src/main/java/net/frozenorb/foxtrot/persist/maps/GlintMap.java
+++ src/main/java/net/frozenorb/foxtrot/persist/maps/GlintMap.java
@@ -1,30 +1,14 @@
 package net.frozenorb.foxtrot.persist.maps;
 
-import net.frozenorb.foxtrot.Foxtrot;
 import net.frozenorb.foxtrot.persist.PersistMap;
-import net.minecraft.server.v1_7_R4.EntityPlayer;
-import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
 
-import java.lang.reflect.Field;
 import java.util.UUID;
 
 public class GlintMap extends PersistMap<Boolean> {
 
     public GlintMap() {
         super("Glint", "Glint");
-
-        Foxtrot.getInstance().getServer().getPluginManager().registerEvents(new Listener() {
-
-            @EventHandler
-            public void onPlayerLogin(PlayerLoginEvent event) {
-                GlintMap.setGlintEnabled(event.getPlayer(), getGlintToggled(event.getPlayer().getUniqueId()));
-            }
-
-        }, Foxtrot.getInstance());
     }
 
     @Override
