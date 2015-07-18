4a35fe4b220923564fc67b22197fcd32ad58f64b
Colin McDonald <macguy8.main@gmail.com>
2015-07-18 14:35:47 -0400

    Add /p alias, send panic message every 15s.

--- src/main/java/net/frozenorb/foxtrot/commands/PanicCommand.java
+++ src/main/java/net/frozenorb/foxtrot/commands/PanicCommand.java
@@ -5,19 +5,31 @@ import net.frozenorb.mBasic.CommandSystem.Commands.Freeze;
 import net.frozenorb.qlib.command.Command;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PanicCommand {
 
-    @Command(names={ "panic" }, permissionNode="foxtrot.panic")
+    @Command(names={ "panic", "p" }, permissionNode="foxtrot.panic")
     public static void panic(Player sender) {
         if (Freeze.isFrozen(sender)) {
             Freeze.unfreeze(sender);
         } else {
-            for (Player player : Foxtrot.getInstance().getServer().getOnlinePlayers()) {
-                if (player.hasPermission("basic.staff")) {
-                    player.sendMessage(ChatColor.RED.toString() + ChatColor.BOLD + "Panic! " + sender.getDisplayName() + ChatColor.YELLOW + " has activated the panic feature!");
+            new BukkitRunnable() {
+
+                public void run() {
+                    if (!Freeze.isFrozen(sender)) {
+                        cancel();
+                        return;
+                    }
+
+                    for (Player player : Foxtrot.getInstance().getServer().getOnlinePlayers()) {
+                        if (player.hasPermission("basic.staff")) {
+                            player.sendMessage(ChatColor.RED.toString() + ChatColor.BOLD + "Panic! " + sender.getDisplayName() + ChatColor.YELLOW + " has activated the panic feature!");
+                        }
+                    }
                 }
-            }
+
+            }.runTaskTimer(Foxtrot.getInstance(), 1L, 15 * 20L);
 
             Freeze.freeze(sender);
         }
