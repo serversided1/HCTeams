11c503d33568667d578739ddb0139f9d46021f99
Colin McDonald <macguy8.main@gmail.com>
2015-07-24 18:52:52 -0400

    Implement the new qLib scoreboard API.

--- src/main/java/net/frozenorb/foxtrot/scoreboard/FoxtrotScoreGetter.java
+++ src/main/java/net/frozenorb/foxtrot/scoreboard/FoxtrotScoreGetter.java
@@ -1,27 +1,158 @@
 package net.frozenorb.foxtrot.scoreboard;
 
+import net.frozenorb.foxtrot.Foxtrot;
+import net.frozenorb.foxtrot.koth.KOTH;
+import net.frozenorb.foxtrot.listener.EnderpearlListener;
+import net.frozenorb.foxtrot.pvpclasses.pvpclasses.ArcherClass;
+import net.frozenorb.foxtrot.pvpclasses.pvpclasses.BardClass;
+import net.frozenorb.foxtrot.server.SpawnTagHandler;
+import net.frozenorb.qlib.scoreboard.ScoreFunction;
 import net.frozenorb.qlib.scoreboard.ScoreGetter;
-import net.frozenorb.qlib.scoreboard.ScoreboardScore;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // Foxtrot has a bunch of constant scores, so this is really simple.
 public class FoxtrotScoreGetter implements ScoreGetter {
 
-    private static ScoreboardScore[] SCORES = new ScoreboardScore[] {
-
-            new SpawnTagScore(),
-            new EnderpearlCooldownScore(),
-            new PvPTimerScore(),
-            new KOTHScore(),
-            new ArcherMarkScore(),
-            new BardEffectScore(),
-            new BardEnergyScore()
-
-    };
-
     @Override
-    public ScoreboardScore[] getScores(Player player) {
-        return (SCORES);
+    public String[] getScores(Player player) {
+        List<String> scores = new ArrayList<>();
+        String spawnTagScore = getSpawnTagScore(player);
+        String enderpearlScore = getEnderpearlScore(player);
+        String pvpTimerScore = getPvPTimerScore(player);
+        String archerMarkScore = getArcherMarkScore(player);
+        String bardEffectScore = getBardEffectScore(player);
+        String bardEnergyScore = getBardEnergyScore(player);
+
+        if (spawnTagScore != null) {
+            scores.add("&c&lSpawn Tag&7: &c" + spawnTagScore);
+        }
+
+        if (enderpearlScore != null) {
+            scores.add("&e&lEnderpearl&7: &c" + enderpearlScore);
+        }
+
+        if (pvpTimerScore != null) {
+            scores.add("&a&lPvP Timer&7: &c" + pvpTimerScore);
+        }
+
+        for (KOTH koth : Foxtrot.getInstance().getKOTHHandler().getKOTHs()) {
+            if (!koth.isActive() || koth.isHidden()) {
+                continue;
+            }
+
+            String displayName;
+
+            switch (koth.getName()) {
+                case "EOTW":
+                    displayName = ChatColor.DARK_RED.toString() + ChatColor.BOLD + "EOTW";
+                    break;
+                case "Citadel":
+                    displayName = ChatColor.DARK_PURPLE.toString() + ChatColor.BOLD + "Citadel";
+                    break;
+                default:
+                    displayName = ChatColor.BLUE.toString() + ChatColor.BOLD + koth.getName();
+                    break;
+            }
+
+            scores.add(displayName + "&7: &c" + ScoreFunction.TIME_SIMPLE.apply((float) koth.getRemainingCapTime()));
+        }
+
+        if (archerMarkScore != null) {
+            scores.add("&6&lArcher Mark&7: &c" + archerMarkScore);
+        }
+
+        if (bardEffectScore != null) {
+            scores.add("&a&lBard Effect&7: &c" + bardEffectScore);
+        }
+
+        if (bardEnergyScore != null) {
+            scores.add("&b&lBard Energy&7: &c" + bardEnergyScore);
+        }
+
+        if (!scores.isEmpty()) {
+            // 'Top' and bottom.
+            scores.add(0, "&7&m--------------------");
+            scores.add("&7&m--------------------");
+        }
+        
+        return (scores.toArray(new String[scores.size()]));
+    }
+
+    public String getSpawnTagScore(Player player) {
+        if (SpawnTagHandler.isTagged(player)) {
+            float diff = SpawnTagHandler.getTag(player);
+
+            if (diff >= 0) {
+                return (ScoreFunction.TIME_FANCY.apply(diff / 1000F));
+            }
+        }
+
+        return (null);
+    }
+
+    public String getEnderpearlScore(Player player) {
+        if (EnderpearlListener.getEnderpearlCooldown().containsKey(player.getName()) && EnderpearlListener.getEnderpearlCooldown().get(player.getName()) >= System.currentTimeMillis()) {
+            float diff = EnderpearlListener.getEnderpearlCooldown().get(player.getName()) - System.currentTimeMillis();
+
+            if (diff >= 0) {
+                return (ScoreFunction.TIME_FANCY.apply(diff / 1000F));
+            }
+        }
+
+        return (null);
+    }
+
+    public String getPvPTimerScore(Player player) {
+        if (Foxtrot.getInstance().getPvPTimerMap().hasTimer(player.getUniqueId())) {
+            int secondsRemaining = Foxtrot.getInstance().getPvPTimerMap().getSecondsRemaining(player.getUniqueId());
+
+            if (secondsRemaining >= 0) {
+                return (ScoreFunction.TIME_SIMPLE.apply((float) secondsRemaining));
+            }
+        }
+
+        return (null);
+    }
+
+    public String getArcherMarkScore(Player player) {
+        if (ArcherClass.isMarked(player)) {
+            long diff = ArcherClass.getMarkedPlayers().get(player.getName()) - System.currentTimeMillis();
+
+            if (diff > 0) {
+                return (ScoreFunction.TIME_FANCY.apply(diff / 1000F));
+            }
+        }
+
+        return (null);
+    }
+
+    public String getBardEffectScore(Player player) {
+        if (BardClass.getLastEffectUsage().containsKey(player.getName()) && BardClass.getLastEffectUsage().get(player.getName()) >= System.currentTimeMillis()) {
+            float diff = BardClass.getLastEffectUsage().get(player.getName()) - System.currentTimeMillis();
+
+            if (diff > 0) {
+                return (ScoreFunction.TIME_SIMPLE.apply(diff / 1000F));
+            }
+        }
+
+        return (null);
+    }
+
+    public String getBardEnergyScore(Player player) {
+        if (BardClass.getEnergy().containsKey(player.getName())) {
+            float energy = BardClass.getEnergy().get(player.getName());
+
+            if (energy > 0) {
+                // No function here, as it's a "raw" value.
+                return (String.valueOf(BardClass.getEnergy().get(player.getName())));
+            }
+        }
+
+        return (null);
     }
 
 }
