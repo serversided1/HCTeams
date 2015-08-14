dbd05a82ae84eae553171268ca806cef70448c45
Colin McDonald <macguy8.main@gmail.com>
2015-08-14 10:38:28 -0400

    Add a utility to import/export team data.

---
+++ src/main/java/net/frozenorb/foxtrot/team/commands/ExportTeamDataCommand.java
@@ -0,0 +1,44 @@
+package net.frozenorb.foxtrot.team.commands;
+
+import net.frozenorb.foxtrot.Foxtrot;
+import net.frozenorb.foxtrot.team.Team;
+import net.frozenorb.qlib.command.Command;
+import net.frozenorb.qlib.command.Parameter;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import java.io.*;
+import java.util.Date;
+
+public class ExportTeamDataCommand {
+
+    @Command(names={ "exportteamdata" }, permissionNode="op")
+    public static void exportTeamData(Player sender, @Parameter(name="file") String fileName) {
+        File file = new File(fileName);
+
+        if (file.exists()) {
+            sender.sendMessage(ChatColor.RED + "An export under that name already exists.");
+            return;
+        }
+
+        try {
+            DataOutputStream out = new DataOutputStream(new FileOutputStream(file));
+
+            out.writeUTF(sender.getName());
+            out.writeUTF(new Date().toString());
+            out.writeInt(Foxtrot.getInstance().getTeamHandler().getTeams().size());
+
+            for (Team team : Foxtrot.getInstance().getTeamHandler().getTeams()) {
+                out.writeUTF(team.getName());
+                out.writeUTF(team.saveString(false));
+            }
+
+            sender.sendMessage(ChatColor.GOLD + "Saved " + Foxtrot.getInstance().getTeamHandler().getTeams().size() + " teams to " + ChatColor.GREEN + file.getAbsolutePath() + ChatColor.GOLD + ".");
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Foxtrot.getInstance().getLogger().severe("Could not import teams! Check console for errors.");
+        }
+    }
+
+}
---
+++ src/main/java/net/frozenorb/foxtrot/team/commands/ImportTeamDataCommand.java
@@ -0,0 +1,47 @@
+package net.frozenorb.foxtrot.team.commands;
+
+import net.frozenorb.foxtrot.Foxtrot;
+import net.frozenorb.foxtrot.team.Team;
+import net.frozenorb.qlib.command.Command;
+import net.frozenorb.qlib.command.Parameter;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import java.io.*;
+
+public class ImportTeamDataCommand {
+
+    @Command(names={ "importteamdata" }, permissionNode="op")
+    public static void importTeamData(Player sender, @Parameter(name="file") String fileName) {
+        File file = new File(fileName);
+
+        if (!file.exists()) {
+            sender.sendMessage(ChatColor.RED + "An export under that name does not exist.");
+            return;
+        }
+
+        try {
+            DataInputStream in = new DataInputStream(new FileInputStream(file));
+            String author = in.readUTF();
+            String created = in.readUTF();
+            int teamsToRead = in.readInt();
+
+            for (int i = 0; i < teamsToRead; i++) {
+                String teamName = in.readUTF();
+                String teamData = in.readUTF();
+
+                Team team = new Team(teamName);
+                team.load(teamData);
+
+                Foxtrot.getInstance().getTeamHandler().setupTeam(team);
+            }
+
+            sender.sendMessage(ChatColor.GOLD + "Loaded " + teamsToRead + " teams from an export created by " + ChatColor.GREEN + author + ChatColor.GOLD + " at " + ChatColor.GREEN + created + ChatColor.GOLD + ".");
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Foxtrot.getInstance().getLogger().severe("Could not import teams! Check console for errors.");
+        }
+    }
+
+}
