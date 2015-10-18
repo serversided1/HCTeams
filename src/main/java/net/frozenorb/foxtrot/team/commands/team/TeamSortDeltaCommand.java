0a85ce71aa507005f6814323b7bc41e7628cb4f6
Colin McDonald <macguy8.main@gmail.com>
2015-10-18 18:41:37 -0400

    Add max/min to the team sort delta.

--- src/main/java/net/frozenorb/foxtrot/team/commands/team/TeamSortDeltaCommand.java
+++ src/main/java/net/frozenorb/foxtrot/team/commands/team/TeamSortDeltaCommand.java
@@ -7,8 +7,21 @@ import org.bukkit.entity.Player;
 
 public class TeamSortDeltaCommand {
 
+    public static final float KB_MAX = 0.5F;
+    public static final float KB_MIN = -0.5F;
+
     @Command(names={ "team sortdelta" }, permissionNode="op")
     public static void teamSortDelta(Player sender, @Parameter(name="team") Player player, @Parameter(name="delta") float mod) {
+        if (mod > KB_MAX) {
+            sender.sendMessage(ChatColor.RED + "The modifier you entered (" + mod + ") was greater than the max (" + KB_MAX + ") - it has been reduced.");
+            mod = KB_MAX;
+        }
+
+        if (mod > KB_MIN) {
+            sender.sendMessage(ChatColor.RED + "The modifier you entered (" + mod + ") was lower than the min (" + KB_MIN + ") - it has been increased.");
+            mod = KB_MIN;
+        }
+
         ((CraftPlayer) player).getHandle().knockbackReduction = mod;
         sender.sendMessage(ChatColor.LIGHT_PURPLE + player.getName() + ChatColor.YELLOW + "'s sort delta is " + ChatColor.LIGHT_PURPLE + mod + ChatColor.YELLOW + ". (Default is 0.2)");
     }
