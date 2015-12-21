2c128171daf690e79c0e5c1abcc46782ce871e64
Francisco Saldanha <francisco.gsaldanha@gmail.com>
2015-12-21 12:23:19 -0200

    Make /forcedisbandall only runnable by the console.

--- src/main/java/net/frozenorb/foxtrot/team/commands/ForceDisbandAllCommand.java
+++ src/main/java/net/frozenorb/foxtrot/team/commands/ForceDisbandAllCommand.java
@@ -4,51 +4,70 @@ import net.frozenorb.foxtrot.Foxtrot;
 import net.frozenorb.foxtrot.team.Team;
 import net.frozenorb.qlib.command.Command;
 import org.bukkit.ChatColor;
-import org.bukkit.conversations.*;
-import org.bukkit.entity.Player;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ForceDisbandAllCommand {
 
+    private static Runnable confirmRunnable;
+
     @Command(names={ "forcedisbandall" }, permissionNode="op")
-    public static void forceDisbandAll(Player sender) {
-        ConversationFactory factory = new ConversationFactory(Foxtrot.getInstance()).withModality(true).withPrefix(new NullConversationPrefix()).withFirstPrompt(new StringPrompt() {
+    public static void forceDisbandAll(CommandSender sender) {
+        if (!(sender instanceof ConsoleCommandSender)) {
+            sender.sendMessage(ChatColor.RED + "No permission.");
+            return;
+        }
 
-            public String getPromptText(ConversationContext context) {
-                return (ChatColor.GREEN  + "Are you sure you want to disband all teams? Type §byes§a to confirm or §cno§a to quit.");
+        confirmRunnable = () -> {
+            List<Team> teams = new ArrayList<>();
+
+            for (Team team : Foxtrot.getInstance().getTeamHandler().getTeams()) {
+                teams.add(team);
             }
 
-            @Override
-            public Prompt acceptInput(ConversationContext cc, String s) {
-                if (s.equalsIgnoreCase("yes")) {
-                    List<Team> teams = new ArrayList<>();
-
-                    for (Team team : Foxtrot.getInstance().getTeamHandler().getTeams()) {
-                        teams.add(team);
-                    }
-
-                    for (Team team : teams) {
-                        team.disband();
-                    }
-
-                    Foxtrot.getInstance().getServer().broadcastMessage(ChatColor.RED.toString() + ChatColor.BOLD + "All teams have been forcibly disbanded!");
-                    return (Prompt.END_OF_CONVERSATION);
-                }
-
-                if (s.equalsIgnoreCase("no")) {
-                    cc.getForWhom().sendRawMessage(ChatColor.GREEN + "Disbanding cancelled.");
-                    return (Prompt.END_OF_CONVERSATION);
-                }
-
-                cc.getForWhom().sendRawMessage(ChatColor.GREEN + "Unrecognized response. Type §b/yes§a to confirm or §c/no§a to quit.");
-                return (Prompt.END_OF_CONVERSATION);
+            for (Team team : teams) {
+                team.disband();
             }
 
-        }).withEscapeSequence("/no").withLocalEcho(false).withTimeout(10).thatExcludesNonPlayersWithMessage("Go away evil console!");
-        Conversation con = factory.buildConversation(sender);
-        sender.beginConversation(con);
+            Foxtrot.getInstance().getServer().broadcastMessage(ChatColor.RED.toString() + ChatColor.BOLD + "All teams have been forcibly disbanded!");
+        };
+
+        sender.sendMessage(ChatColor.RED + "Are you sure you want to disband all factions? Type " + ChatColor.DARK_RED + "/forcedisbandall confirm" + ChatColor.RED + " to confirm or " + ChatColor.GREEN + "/forcedisbandall cancel" + ChatColor.RED +" to cancel.");
+    }
+
+    @Command(names = {"forcedisbandall confirm"}, permissionNode = "op")
+    public static void confirm(CommandSender sender) {
+        if (!(sender instanceof ConsoleCommandSender)) {
+            sender.sendMessage(ChatColor.RED + "No permission.");
+            return;
+        }
+
+        if (confirmRunnable == null) {
+            sender.sendMessage(ChatColor.RED + "Nothing to confirm.");
+            return;
+        }
+
+        sender.sendMessage(ChatColor.GREEN + "If you're sure...");
+        confirmRunnable.run();
+    }
+
+    @Command(names = {"forcedisbandall cancel"}, permissionNode = "op")
+    public static void cancel(CommandSender sender) {
+        if (!(sender instanceof ConsoleCommandSender)) {
+            sender.sendMessage(ChatColor.RED + "No permission.");
+            return;
+        }
+
+        if (confirmRunnable == null) {
+            sender.sendMessage(ChatColor.RED + "Nothing to cancel.");
+            return;
+        }
+
+        sender.sendMessage(ChatColor.GREEN + "Cancelled.");
+        confirmRunnable = null;
     }
 
 }
