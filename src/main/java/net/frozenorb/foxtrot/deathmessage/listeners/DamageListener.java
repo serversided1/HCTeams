86deaa96cf1848c63c7a92b4892af41d30decbdb
Francisco Saldanha <francisco.gsaldanha@gmail.com>
2016-01-31 15:04:22 -0200

    Actually check if they want to see the death message before sending it

--- src/main/java/net/frozenorb/foxtrot/deathmessage/listeners/DamageListener.java
+++ src/main/java/net/frozenorb/foxtrot/deathmessage/listeners/DamageListener.java
@@ -7,6 +7,7 @@ import net.frozenorb.foxtrot.deathmessage.objects.Damage;
 import net.frozenorb.foxtrot.deathmessage.objects.PlayerDamage;
 import net.frozenorb.foxtrot.deathmessage.util.UnknownDamage;
 import net.frozenorb.foxtrot.deathtracker.DeathTracker;
+import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -34,6 +35,10 @@ public class DamageListener implements Listener {
     public void onPlayerDeath(PlayerDeathEvent event) {
         List<Damage> record = DeathMessageHandler.getDamage(event.getEntity());
 
+        event.setDeathMessage(null);
+
+        String deathMessage;
+
         if (record != null) {
             Damage deathCause = record.get(record.size() - 1);
 
@@ -48,9 +53,15 @@ public class DamageListener implements Listener {
                 }
             }
 
-            event.setDeathMessage(deathCause.getDeathMessage());
+            deathMessage = deathCause.getDeathMessage();
         } else {
-            event.setDeathMessage((new UnknownDamage(event.getEntity().getName(), 1)).getDeathMessage());
+            deathMessage = new UnknownDamage(event.getEntity().getName(), 1).getDeathMessage();
+        }
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (Foxtrot.getInstance().getToggleDeathMessageMap().areDeathMessagesEnabled(player.getUniqueId())) {
+                player.sendMessage(deathMessage);
+            }
         }
 
         DeathTracker.logDeath(event.getEntity(), event.getEntity().getKiller());
