e6398c8de78801d8aef230e44a1cf24b3a69cad8
Nick Porillo <nsporillo@gmail.com>
2016-03-26 22:27:23 -0700

    Add reference /kit command
    Just outputs the exact same thing as the help. Many (and i encountered
    a few myself) use /kit to find out info and are confused.
    
    This command should never need to be updated as it uses the enchantment
    enumerations max value, which is set somewhere else on a map per map
    basis.

---
+++ src/main/java/net/frozenorb/foxtrot/commands/KitCommand.java
@@ -0,0 +1,16 @@
+package net.frozenorb.foxtrot.commands;
+
+import net.frozenorb.qlib.command.Command;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+
+public class KitCommand {
+
+    @Command(names = "kit", permissionNode = "")
+    public static void kit(Player sender) {
+        String sharp = "Sharpness " + Enchantment.DAMAGE_ALL.getMaxLevel();
+        String prot = "Protection " + Enchantment.PROTECTION_ENVIRONMENTAL.getMaxLevel();
+        String bow = "Power " + Enchantment.ARROW_DAMAGE.getMaxLevel();
+        sender.sendMessage("§eEnchant Limits: §7" + sharp + ", " + prot + ", " + bow);
+    }
+}
