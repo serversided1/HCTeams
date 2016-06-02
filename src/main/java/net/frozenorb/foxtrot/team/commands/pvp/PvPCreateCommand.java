ec942be2ad301b84226b458f763a7f0cd17218a2
Jonathan Halterman <jhalterman@minehq.com>
2016-06-02 08:46:10 -0700

    Add pvp prot create aliases

--- src/main/java/net/frozenorb/foxtrot/team/commands/pvp/PvPCreateCommand.java
+++ src/main/java/net/frozenorb/foxtrot/team/commands/pvp/PvPCreateCommand.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class PvPCreateCommand {
 
-    @Command(names={ "pvptimer create", "timer create", "pvp create" }, permission="worldedit.*")
+    @Command(names={ "pvptimer create", "timer create", "pvp create", "pvp add", "pvp give" }, permission="worldedit.*")
     public static void pvpCreate(Player sender, @Param(name="player", defaultValue="self") Player player) {
         Foxtrot.getInstance().getPvPTimerMap().createTimer(player.getUniqueId(), (int) TimeUnit.MINUTES.toSeconds(30));
         player.sendMessage(ChatColor.YELLOW + "You have 30 minutes of PVP Timer!");
