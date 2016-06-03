31729378fa5ca80b87df2dbd8a5e74b74ec17bd7
Jonathan Halterman <jhalterman@minehq.com>
2016-06-02 22:36:58 -0700

    sk89q's code was probably old. Return proper cardinal directions.

--- src/main/java/net/frozenorb/foxtrot/util/PlayerDirection.java
+++ src/main/java/net/frozenorb/foxtrot/util/PlayerDirection.java
@@ -21,23 +21,23 @@ public class PlayerDirection {
      */
     private static String getDirection(double rot) {
         if (0 <= rot && rot < 22.5) {
-            return "N";
-        } else if (22.5 <= rot && rot < 67.5) {
-            return "NE";
-        } else if (67.5 <= rot && rot < 112.5) {
-            return "E";
-        } else if (112.5 <= rot && rot < 157.5) {
-            return "SE";
-        } else if (157.5 <= rot && rot < 202.5) {
-            return "S";
-        } else if (202.5 <= rot && rot < 247.5) {
-            return "SW";
-        } else if (247.5 <= rot && rot < 292.5) {
             return "W";
-        } else if (292.5 <= rot && rot < 337.5) {
+        } else if (22.5 <= rot && rot < 67.5) {
             return "NW";
-        } else if (337.5 <= rot && rot < 360.0) {
+        } else if (67.5 <= rot && rot < 112.5) {
             return "N";
+        } else if (112.5 <= rot && rot < 157.5) {
+            return "NE";
+        } else if (157.5 <= rot && rot < 202.5) {
+            return "E";
+        } else if (202.5 <= rot && rot < 247.5) {
+            return "SE";
+        } else if (247.5 <= rot && rot < 292.5) {
+            return "S";
+        } else if (292.5 <= rot && rot < 337.5) {
+            return "SW";
+        } else if (337.5 <= rot && rot < 360.0) {
+            return "W";
         } else {
             return null;
         }
