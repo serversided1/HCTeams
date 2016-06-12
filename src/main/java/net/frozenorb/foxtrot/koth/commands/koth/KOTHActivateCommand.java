c9ba1013f2f802e8b466ba17ae3a4e5c858d55d3
Colin McDonald <cmcdonald.main@gmail.com>
2016-06-12 15:24:23 -0400

    Don't let admins active a koth if another koth is already active

--- src/main/java/net/frozenorb/foxtrot/koth/commands/koth/KOTHActivateCommand.java
+++ src/main/java/net/frozenorb/foxtrot/koth/commands/koth/KOTHActivateCommand.java
@@ -1,5 +1,6 @@
 package net.frozenorb.foxtrot.koth.commands.koth;
 
+import net.frozenorb.foxtrot.Foxtrot;
 import net.frozenorb.foxtrot.koth.KOTH;
 import net.frozenorb.qlib.command.Command;
 import net.frozenorb.qlib.command.Param;
@@ -10,6 +11,13 @@ public class KOTHActivateCommand {
 
     @Command(names={ "KOTH Activate", "KOTH Active" }, permission="foxtrot.koth")
     public static void kothActivate(Player sender, @Param(name="koth") KOTH koth) {
+        // Don't start a KOTH if another one is active.
+        for (KOTH koth : Foxtrot.getInstance().getKOTHHandler().getKOTHs()) {
+            if (koth.isActive()) {
+                return;
+            }
+        }
+
         koth.activate();
         sender.sendMessage(ChatColor.GRAY + "Activated " + koth.getName() + ".");
     }
