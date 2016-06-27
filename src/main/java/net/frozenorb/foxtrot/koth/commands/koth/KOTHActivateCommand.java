64916389cd4bd5a9fe18f056b310e8a907b4f146
Guru_Fraser <frazz86@googlemail.com>
2016-06-27 22:39:54 +0100

    Don't let admins activate Citadel. Closes #56

--- src/main/java/net/frozenorb/foxtrot/koth/commands/koth/KOTHActivateCommand.java
+++ src/main/java/net/frozenorb/foxtrot/koth/commands/koth/KOTHActivateCommand.java
@@ -19,6 +19,10 @@ public class KOTHActivateCommand {
             }
         }
 
+        if( (koth.getName().equalsIgnoreCase("citadel") || koth.getName().toLowerCase().contains("conquest")) && !sender.isOp()) {
+            sender.sendMessage(ChatColor.RED + "Only ops can use the activate command for citadel.");
+            return;
+        }
         koth.activate();
         sender.sendMessage(ChatColor.GRAY + "Activated " + koth.getName() + ".");
     }
