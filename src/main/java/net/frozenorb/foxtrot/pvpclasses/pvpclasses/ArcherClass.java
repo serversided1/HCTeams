d301c44aaf712d5ae197a53b283596d3110e11f8
Jonathan Halterman <jhalt35@gmail.com>
2016-08-01 17:19:10 -0700

    Update Archer to have Resistance 2

--- src/main/java/net/frozenorb/foxtrot/pvpclasses/pvpclasses/ArcherClass.java
+++ src/main/java/net/frozenorb/foxtrot/pvpclasses/pvpclasses/ArcherClass.java
@@ -48,7 +48,7 @@ public class ArcherClass extends PvPClass {
     @Override
     public void apply(Player player) {
         player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 2), true);
-        player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, Integer.MAX_VALUE, 0), true);
+        player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, Integer.MAX_VALUE, 1), true);
     }
 
     @Override
@@ -58,7 +58,7 @@ public class ArcherClass extends PvPClass {
         }
 
         if (!player.hasPotionEffect(PotionEffectType.DAMAGE_RESISTANCE)) {
-            player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, Integer.MAX_VALUE, 0));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, Integer.MAX_VALUE, 1));
         }
         super.tick(player);
     }
