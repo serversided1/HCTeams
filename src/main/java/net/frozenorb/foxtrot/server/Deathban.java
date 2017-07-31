5dea051a51be2b90923bcdf14fdf41c98ada1ef9
Alfie Cleveland <alfeh@me.com>
2017-07-31 18:30:55 +0100

    Slightly improved deathban code

--- src/main/java/net/frozenorb/foxtrot/server/Deathban.java
+++ src/main/java/net/frozenorb/foxtrot/server/Deathban.java
@@ -10,9 +10,9 @@ import java.util.concurrent.TimeUnit;
 public final class Deathban {
 
     private static Map<String, Integer> deathban = new LinkedHashMap<>();
+    private static int defaultMinutes = 240;
 
     static {
-        deathban.put("DEFAULT", 240);
         deathban.put("VIP", 120);
         deathban.put("PRO", 90);
         deathban.put("EPIC", 45);
@@ -23,18 +23,20 @@ public final class Deathban {
         deathban.clear();
 
         for (String key : object.keySet()) {
-            deathban.put(key, object.getInt(key));
+            if (key.equals("DEFAULT"))  {
+                defaultMinutes = object.getInt(key);
+            } else {
+                deathban.put(key, object.getInt(key));
+            }
         }
     }
 
     public static int getDeathbanSeconds(Player player) {
-        int minutes = 0;
+        int minutes = defaultMinutes;
 
         for (Map.Entry<String, Integer> entry : deathban.entrySet()) {
-            if (entry.getKey().equals("DEFAULT") || player.hasPermission("inherit." + entry.getKey().toLowerCase())) {
-                if (minutes == 0 || minutes > entry.getValue()) {
-                    minutes = entry.getValue();
-                }
+            if (player.hasPermission("inherit." + entry.getKey().toLowerCase()) && entry.getValue() < minutes) {
+                minutes = entry.getValue();
             }
         }
 
