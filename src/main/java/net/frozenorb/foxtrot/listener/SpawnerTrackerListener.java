4937d973d541b0a2bd276e96b5918471b79e8929
Joel Evans <joel@minexd.com>
2019-03-08 06:56:52 -0500

    Move some spawner tracking code from listener to proper team upgrade class

--- src/main/java/net/frozenorb/foxtrot/listener/SpawnerTrackerListener.java
+++ src/main/java/net/frozenorb/foxtrot/listener/SpawnerTrackerListener.java
@@ -5,29 +5,20 @@ import net.frozenorb.foxtrot.team.Team;
 import net.frozenorb.foxtrot.team.claims.Claim;
 import net.frozenorb.foxtrot.team.claims.LandBoard;
 import net.frozenorb.foxtrot.team.event.CrowbarSpawnerBreakEvent;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 public class SpawnerTrackerListener implements Listener {
 
-	@EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+	@EventHandler(ignoreCancelled = true)
 	public void onBlockPlaceEvent(BlockPlaceEvent event) {
 		if (event.getBlockPlaced().getType() == Material.MOB_SPAWNER) {
 			Team team = Foxtrot.getInstance().getTeamHandler().getTeam(event.getPlayer());
 
 			if (team != null) {
-				if (team.getSpawnersInClaim() >= 5 + team.getExtraSpawners()) {
-					event.setCancelled(true);
-					event.getPlayer().sendMessage(ChatColor.RED + "You can't place more than " + (5 + team.getExtraSpawners()) + " spawners in your claim.");
-					event.getPlayer().sendMessage(ChatColor.RED + "To bypass this limit, you can purchase team upgrades via /t upgrades.");
-					return;
-				}
-
 				Claim claim = LandBoard.getInstance().getClaim(event.getBlockPlaced().getLocation());
 
 				if (claim != null && team.getClaims().contains(claim)) {
@@ -37,7 +28,7 @@ public class SpawnerTrackerListener implements Listener {
 		}
 	}
 
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	@EventHandler(ignoreCancelled = true)
 	public void onBlockBreakEvent(BlockBreakEvent event) {
 		if (event.getBlock().getType() == Material.MOB_SPAWNER) {
 			Team team = Foxtrot.getInstance().getTeamHandler().getTeam(event.getPlayer());
@@ -52,7 +43,7 @@ public class SpawnerTrackerListener implements Listener {
 		}
 	}
 
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	@EventHandler(ignoreCancelled = true)
 	public void onSpawnerBreakEvent(CrowbarSpawnerBreakEvent event) {
 		Team team = Foxtrot.getInstance().getTeamHandler().getTeam(event.getPlayer());
 
--- src/main/java/net/frozenorb/foxtrot/team/upgrades/impl/ExtraSpawnerTeamUpgrade.java
+++ src/main/java/net/frozenorb/foxtrot/team/upgrades/impl/ExtraSpawnerTeamUpgrade.java
@@ -1,10 +1,18 @@
 package net.frozenorb.foxtrot.team.upgrades.impl;
 
+import net.frozenorb.foxtrot.Foxtrot;
+import net.frozenorb.foxtrot.team.Team;
+import net.frozenorb.foxtrot.team.claims.Claim;
 import net.frozenorb.foxtrot.team.upgrades.TeamUpgrade;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class ExtraSpawnerTeamUpgrade implements TeamUpgrade {
+public class ExtraSpawnerTeamUpgrade implements TeamUpgrade, Listener {
 
 	@Override
 	public String getUpgradeName() {
@@ -29,6 +37,29 @@ public class ExtraSpawnerTeamUpgrade implements TeamUpgrade {
 	@Override
 	public int getTierLimit() {
 		return 5;
+	}
+
+	@EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+	public void onBlockPlaceEvent(BlockPlaceEvent event) {
+		if (event.getBlockPlaced().getType() == Material.MOB_SPAWNER) {
+			Team team = Foxtrot.getInstance().getTeamHandler().getTeam(event.getPlayer());
+
+			if (team != null) {
+				for (Claim claim : team.getClaims()) {
+					if (claim.contains(event.getBlockPlaced().getLocation())) {
+						int spawnerLimit = 5 + getTier(team);
+
+						if (team.getSpawnersInClaim() >= spawnerLimit) {
+							event.setCancelled(true);
+							event.getPlayer().sendMessage(ChatColor.RED + "You can't place more than " + spawnerLimit + " spawners in your claim.");
+							event.getPlayer().sendMessage(ChatColor.RED + "To bypass this limit, you can purchase team upgrades via /t upgrades.");
+						}
+
+						break;
+					}
+				}
+			}
+		}
 	}
 
 }
