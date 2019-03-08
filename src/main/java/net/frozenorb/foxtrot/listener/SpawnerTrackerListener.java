f9393fe12f310dcea362c3a81d5ec975f014d603
Joel Evans <joel@minexd.com>
2019-03-07 23:05:23 -0500

    Fixed spawner count being increment for all block placements in claim? (whoops)

--- src/main/java/net/frozenorb/foxtrot/listener/SpawnerTrackerListener.java
+++ src/main/java/net/frozenorb/foxtrot/listener/SpawnerTrackerListener.java
@@ -5,6 +5,7 @@ import net.frozenorb.foxtrot.team.Team;
 import net.frozenorb.foxtrot.team.claims.Claim;
 import net.frozenorb.foxtrot.team.claims.LandBoard;
 import net.frozenorb.foxtrot.team.event.CrowbarSpawnerBreakEvent;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -15,13 +16,15 @@ public class SpawnerTrackerListener implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onBlockPlaceEvent(BlockPlaceEvent event) {
-		Team team = Foxtrot.getInstance().getTeamHandler().getTeam(event.getPlayer());
+		if (event.getBlockPlaced().getType() == Material.MOB_SPAWNER) {
+			Team team = Foxtrot.getInstance().getTeamHandler().getTeam(event.getPlayer());
 
-		if (team != null) {
-			Claim claim = LandBoard.getInstance().getClaim(event.getBlockPlaced().getLocation());
+			if (team != null) {
+				Claim claim = LandBoard.getInstance().getClaim(event.getBlockPlaced().getLocation());
 
-			if (claim != null && team.getClaims().contains(claim)) {
-				team.addSpawnersInClaim(1);
+				if (claim != null && team.getClaims().contains(claim)) {
+					team.addSpawnersInClaim(1);
+				}
 			}
 		}
 	}
